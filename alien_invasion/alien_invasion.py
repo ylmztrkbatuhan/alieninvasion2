06c2dacbc01283b299878caca75edc558ab67dbb
oboy21 <oboy11b@gmail.com>
2025-06-01 16:55:05 +0300

    init repo

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,57 @@
+import sys
+from settings import Settings
+from ship import Ship
+import pygame
+
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """initialize the game , and create game resourches."""
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+
+        self.ship = Ship(self)
+
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            self._check_events()
+            # Watch keyboard and mouse events
+            self._update_screen()
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                    # Move the ship to the right.
+
+
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+            #Redraw the screen during each pass  through the loop.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        #make the most recently drawn screen visible.
+        pygame.display.flip()
+
+
+
+
+if __name__ == "__main__":
+    #Make a game instance, and run the game
+    ai = AlienInvasion()
+    ai.run_game()
